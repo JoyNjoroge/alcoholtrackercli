8915019faab7049bff0e56eb9048de26bed05403
JoyNjoroge <njorogejoy78@gmail.com>
2025-05-29 23:48:00 +0300

    Units.py

--- lib/units.py
+++ lib/units.py
@@ -1,11 +1,10 @@
-# units.py
 def convert_to_oz(amount, unit):
     """Convert different units to ounces"""
     unit = unit.lower()
     if unit == "ml":
-        return amount * 0.033814  # 1 ml = 0.033814 oz
+        return amount * 0.033814
     elif unit == "l":
-        return amount * 33.814  # 1 liter = 33.814 oz
+        return amount * 33.814
     elif unit == "oz":
         return amount
     else:
